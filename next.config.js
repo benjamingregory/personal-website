dfb72d377e755a0b4adf638a4e3bf78d2fd700b9
Ben Gregory <benjaminrgregory@gmail.com>
2025-03-26 10:34:37 -0700

    stuff

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withMDX = require("@next/mdx")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  images: {
+    domains: ["www.youtube.com", "img.youtube.com"],
+  },
 };
 
 module.exports = withMDX(nextConfig);
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -7,7 +7,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h2: ({ children }) => <h2 style={{ fontSize: "36px" }}>{children}</h2>,
     h3: ({ children }) => <h3 style={{ fontSize: "24px" }}>{children}</h3>,
     h4: ({ children }) => <h4 style={{ fontSize: "18px" }}>{children}</h4>,
-    ul: (props: any) => <ul className="list-disc" {...props} />,
+    ul: (props: any) => <ul className="text-sm list-disc" {...props} />,
     p: (props: any) => <p className="text-sm" {...props} />,
     a: (props: any) => <a className="font-bold " {...props} />,
     img: (props: any) => (
